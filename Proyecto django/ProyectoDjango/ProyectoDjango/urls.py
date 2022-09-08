cc0f13551d817351be59c033fd9c200b6506946b
Juan Jose Martinez <josemargri3@gmail.com>
2022-09-08 01:00:01 -0500

    creamos la layout y las vistas basicas que heredan la layout

--- Proyecto django/ProyectoDjango/ProyectoDjango/urls.py
+++ Proyecto django/ProyectoDjango/ProyectoDjango/urls.py
@@ -15,7 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from mainapp import views
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path ('',views.index,name='inicio'),
+    path ('inicio/',views.index,name='index'),
+    path('sobre-nosotros/',views.about,name='about')
+
 ]
